105065a485fae0618ab8fb609ea736c3f12547d2
yuan900133 <1987304903@qq.com>
2016-06-04 23:26:29 -0700

    项目初始化

---
+++ WCIBKnowledge/WCIBKnowledge/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  WCIBKnowledge
+//
+//  Created by 袁武昌 on 16/6/4.
+//  Copyright © 2016年 yuan.com. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
